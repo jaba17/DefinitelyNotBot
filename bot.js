1274f70602aa5e14648b7579027e40eb27b51c0b
BigDaddyMC <37113017+BigDaddyMC@users.noreply.github.com>
2018-03-10 18:05:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.includes('discord.gg/') {
+    if (message.includes('discord.gg') {
     	message.reply('BONG!');
   	}
 });
